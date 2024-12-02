eeb37cb11394f4f121a2c095eaf6e83eda1523e1
honouredev <ganiyahmutohir@gmail.com>
2024-12-02 10:37:03 +0100

    my portfolio page

---
+++ portfolio.js
@@ -0,0 +1,48 @@
+
+const closeIcon = document.getElementById("close");
+const nav = document.querySelector("nav");
+const overlay = document.querySelector(".overlay");
+const menuIcon = document.querySelector("img.menu");
+
+closeIcon.onclick = function() {
+nav.style.transform = "translateX(100%)";
+overlay.style.display = "none";
+}
+
+menuIcon.onclick = function() {
+nav.style.transform = "translateX(0%)";
+overlay.style.display = "block";
+}
+
+
+
+//let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementsByClassName("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
